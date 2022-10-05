412719a6c1da2b8c712f96bc9b50f3a734480d06
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2022-10-05 15:07:18 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const loadProfileImage = () => {
+    const imageEl = document.querySelector('.hero__image');
+    const url = 'https://via.placeholder.com/600x400/707070/969696.png';
+    let preloadImage = new Image();
+    preloadImage.src = url;
+
+    preloadImage.addEventListener('load', () => {
+        imageEl.style.backgroundImage = `url("${url}")`;
+        preloadImage = null;
+        imageEl.classList.add('loaded');
+    });
+}
+
+loadProfileImage();
